d9e20704ea60458a3f37089842efc22a2d76888c
joypauls <joypaulsen3@gmail.com>
2024-09-01 00:43:13 -0500

    attempting a couple characteristic classifications

--- scripts/object_detection.py
+++ scripts/object_detection.py
@@ -2,7 +2,8 @@ import cv2
 import numpy as np
 
 
-MIN_CARD_AREA = 1000  # Adjust these values based on the expected size of the cards
+MIN_CARD_AREA = 1000
+MIN_SHAPE_AREA = 50
 # max_area = 5000
 
 
@@ -28,9 +29,10 @@ def order_points(pts):
 
 
 image = cv2.imread("./images/test_image.jpg")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2LUV)[:, :, 0]
+display_image(image, "Input Image")
 
-display_image(gray, "Grayscale Image")
+
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2LUV)[:, :, 0]
 
 # binary thresholding
 _, binary_image = cv2.threshold(gray, 200, 255, cv2.THRESH_BINARY)
@@ -47,7 +49,8 @@ for contour in contours:
         epsilon = 0.02 * cv2.arcLength(contour, True)
         approx = cv2.approxPolyDP(contour, epsilon, True)
 
-        if len(approx) == 4:  # If the polygon has 4 points, it's likely a card
+        # look for rectangles
+        if len(approx) == 4:
             cards.append(approx)
 
 print(f"Detected {len(cards)} cards")
@@ -56,15 +59,13 @@ print(f"Detected {len(cards)} cards")
 image_copy = image.copy()
 for card in cards:
     cv2.drawContours(image_copy, [card], -1, (0, 0, 255), 5)
-
 display_image(image_copy, "Cards with Contours")
 
+# perspective transform
 flat_cards = []
 for i, card in enumerate(cards):
     pts = card.reshape(4, 2)
-    rect = order_points(
-        pts
-    )  # order points to top-left, top-right, bottom-right, bottom-left
+    rect = order_points(pts)
 
     # determine width and height of the new image
     (tl, tr, br, bl) = rect
@@ -82,16 +83,66 @@ for i, card in enumerate(cards):
         dtype="float32",
     )
 
-    # perspective transform
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
 
     flat_cards.append(warped)
 
-# display each card after perspective transform
-for i, warped in enumerate(flat_cards):
-    # cv2.imwrite(f"card_{i}.jpg", warped)
-    display_image(warped, f"Card {i + 1}/{len(flat_cards)}")
+# # display each card after perspective transform
+# for i, card in enumerate(flat_cards):
+#     # cv2.imwrite(f"card_{i}.jpg", card)
+#     print(card.shape)
+#     display_image(card, f"Card {i + 1}/{len(flat_cards)}")
 
-for i, warped in enumerate(flat_cards):
-    pass
+# detect shapes in each card
+for i, flat_card in enumerate(flat_cards):
+    # binarize card
+    gray_card = cv2.cvtColor(flat_card, cv2.COLOR_BGR2LUV)[:, :, 0]
+    _, binary_card = cv2.threshold(
+        gray_card, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU
+    )
+    # display_image(binary_card, "Binary Card")
+
+    # countours
+    contours, _ = cv2.findContours(
+        cv2.bitwise_not(binary_card), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE
+    )
+    shapes = []
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if MIN_SHAPE_AREA <= area:
+            # shapes.append(contour)
+
+            # approximate contour to polygon
+            epsilon = 0.02 * cv2.arcLength(contour, True)
+            approx = cv2.approxPolyDP(contour, epsilon, True)
+
+            shapes.append(approx)
+
+            # # look for rectangles
+            # if len(approx) == 4:
+            #     shapes.append(approx)
+
+    card_copy = flat_card.copy()
+    for shape in shapes:
+        cv2.drawContours(card_copy, [shape], -1, (0, 255, 0), 3)
+
+    shape_classifications = []
+    for shape in shapes:
+        shape_classification = "UNKNOWN"
+        if len(shape) == 4:
+            shape_classification = "diamond"
+        elif len(shape) == 8:
+            shape_classification = "oval"
+        elif len(shape) == 10:
+            shape_classification = "squiggle"
+
+    if len(set(shape_classifications)) > 1:
+        raise ValueError(
+            f"Inconsistent shapes detected in card {i + 1}/{len(flat_cards)}"
+        )
+
+    display_image(
+        card_copy,
+        f"Card {i + 1}/{len(flat_cards)} ({len(shapes)}, {shape_classification})",
+    )
